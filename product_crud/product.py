d30051e1f876e21eda0f1081b2ad7b3993286f38
sarah bagheri <95619163+Sarahbaqeri@users.noreply.github.com>
2023-01-04 14:22:31 -0800

    modified update and delete and read method

--- product_crud/product.py
+++ product_crud/product.py
@@ -1,14 +1,13 @@
 from datetime import datetime
-
+import uuid
+from product_inmemory_db import ProductInMemoryDb
 class Product():
-
-    _product_list = {}
 
     def __init__(self, title:str, short_description:str , description:str  , slug:str, permalink:str, sku:str, price:float, regular_price:float,
                  sale_price:float, manage_stock:bool, stock_quantity:int, date_created_gmt :int, date_modified_gmt:int,category_id:int = 0, 
                  is_visible = True, is_available:bool = False):
 
-        self.id = None
+        self.id = uuid.uuid1()
         self.category_id = category_id
         self.title = title
         self.short_description =  short_description
@@ -27,31 +26,46 @@ class Product():
         self.date_modified_gmt = date_modified_gmt
        
 
-    def create(self):
-        self._product_list[self] = self.id
+    def create(self,id:int):
+        {'id':{self.id}, 'Title': {self.title},
+        'Short_description': {self.short_description},
+        'Description': {self.description},
+        'Slug': {self.slug},
+        'Permanent_link': {self.permalink},
+        'Stock_keeping_Unit': {self.sku},
+        'Price': {self.price},
+        'Reqular_Price': {self.regular_price},
+        'Sale_Price': {self.sale_price},
+        'Manage_Stock' :{self.manage_stock},
+        'Stock_Quantity': {self.stock_quantity},
+        'Visible': {self.is_visible},'Date_Created':43,'Date_Modified':43}
         return self.__repr__()
 
     #this method shall be able read a product via id/uuid or ... from the the product datastructure (dictionary,list or maybe database)
     def read(self,id):
-        for value,key in self._product_list.items():
-            if value==id:
-                return key
+       for item in ProductInMemoryDb.product_list:
+        if item['id']== id :
+            return item
+        return 'item not found'
 
     #this method shall be able to update product and amend the data structure for related product
-    def update(self):
-        self._product_list[self].update(self.create())
-
+    def update(self,id:int,**attributes):
+       for item in ProductInMemoryDb.product_list:
+        if item['id']== id:
+            item.__init__(**attributes)
+            return attributes
     #this method shall be able to remove the product
-    def delete(self):
-        pass
-
+    def delete(self,id:int):
+        for item in ProductInMemoryDb.product_list:
+            if item['id']== id :
+                ProductInMemoryDb.product_list.remove(item)
+                
     #shall I get all products with staticmethod ? any better solution ? what about a class method ?
     # what is the diffrence ?
     # shall I seprate the datastructe from the class ? why? who? any better solution?
     #@staticmethod
     def list_all(self):
         return tuple(Product._product_list.keys())
-
 
     def __repr__(self) -> str:
         return f"the product with \n\
