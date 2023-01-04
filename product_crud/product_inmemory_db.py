29d537850036da79fe7e1466b450d67d0694b783
sarah bagheri <95619163+Sarahbaqeri@users.noreply.github.com>
2023-01-04 14:42:36 -0800

    create find_by_id method to the class to find and read products by id from product_list

--- product_crud/product_inmemory_db.py
+++ product_crud/product_inmemory_db.py
@@ -3,5 +3,10 @@
 
 class ProductInMemoryDb:
     product_list=[]
-    def insert(self,dict:dict):
+    def add(self,dict:dict):
         ProductInMemoryDb.product_list.append(dict)
+    def update(self,id:int,dict:dict):
+        for item in ProductInMemoryDb.product_list:
+            if item['id']==id:
+                item.insert(dict)
+    
