47b640ddce0969b13cfdb4b746551746cc9d0fc5
amirmortezaee1 <amir.mortezaee1@gmail.com>
2023-01-30 17:32:05 +0330

    create product_injson_db

---
+++ product_crud/product_injson_db.py
@@ -0,0 +1,43 @@
+import json
+
+class InJson:
+
+    def __init__(self, file_path) -> None:
+        self.file_path = file_path
+
+    def add(self,dict) -> None:
+        items=self.open_file()
+        items.append(dict)
+    
+        with open(self.file_path, 'w') as f:
+            json.dump(items,f)
+
+
+    def update(self,dict,id:int) -> None:
+            items=self.read_file()
+            for item in items:
+                if item['id'] == id:
+                    items.remove(item)
+                    items.append(dict)
+                    
+            with open (self.file_path, 'w') as f:
+                json.dump(items,f)
+
+    def read(self) -> dict:
+        items=self.read_file()
+        for item in items :
+                if item['id']== id:
+                    return item
+
+    def remove(self,id:int) -> None:
+        items=self.read_file()
+        for item in items :
+            if item['id']== id:
+                items.remove(item)
+
+        with open(self.file_path,'w') as f:
+            json.dump(items,f)
+
+    def read_file(self) -> list:
+        with open (self.file_path, 'r') as f :
+            return list(json.load(f))
